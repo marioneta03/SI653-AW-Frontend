c75423f4cc9d793d4d04cbc4370025a6a811166b
Jaime Alonso Garrido Guevara <alonso.garrido.2901@hotmail.com>
2020-11-26 23:24:59 -0500

    Update Terms.vue

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -79,8 +79,79 @@
                             state, federal, or local registry.</h4>
 
 
-                        <h2 style="margin-top: 20px; margin-bottom: 5px">3. ....</h2>
-                        <h4 style="font-weight: normal">...</h4>
+                        <h2 style="margin-top: 20px; margin-bottom: 5px">3. Your account.</h2>
+                        <h4 style="font-weight: normal">In order to use GoingTo, you may sign in using your Facebook login.  If you do so, you authorize us to access and use certain Facebook account information, including but not limited to your public Facebook profile. For more information regarding the information we collect from you and how we use it, please consult our Privacy Policy.
+
+You are responsible for maintaining the confidentiality of your login credentials you use to sign up for GoingTo, and you are solely responsible for all activities that occur under those credentials. If you think someone has gained access to your account, please immediately contact us.</h4>
+                        
+                        <h2 style="margin-top: 20px; margin-bottom: 5px">4. Modification of the Services
+                                    and cancellation.</h2>
+                        <h4 style="font-weight: normal">GoingTo is always striving to improve the Service and bring you additional functionality that you will find engaging and useful. This means we may add new product features or enhancements from time to time as well as remove some features, and if these actions do not materially affect your rights or obligations, we may not provide you with notice before taking them. We may even suspend the Service entirely, in which event we will notify you in advance unless extenuating circumstances, such as safety or security concerns, prevent us from doing so.
+
+You may terminate your account at any time, for any reason, by following the instructions in "Settings" in the Service, however if you use a third party payment account, you will need to manage in app purchases through such account (e.g., iTunes, Google Play) to avoid additional billing. GoingTo may terminate your account at any time without notice if it believes that you have violated this Agreement. Upon such termination, you will not be entitled to any refund for purchases. After your account is terminated, this Agreement will terminate, except that the following provisions will still apply to you and GoingTo: Section 4, Section 5, and Sections 12 through 19.</h4>
+                        
+                        <h2 style="margin-top: 20px; margin-bottom: 5px">5. Security; your interactions
+                                    with other users.</h2>
+                        <h4 style="font-weight: normal">Though GoingTo strives to encourage a respectful member experience through features like the double opt-in that allows members to communicate only after they have both indicated interest in one another, it is not responsible for the conduct of any member on or off of the Service. You agree to use caution in all interactions with other members, particularly if you decide to communicate off the Service or meet in person. In addition, you agree to review and follow GoingTo’s Safety Tips prior to using the Service. You agree that you will not provide your financial information (for example, your credit card or bank account information), or wire or otherwise send money, to other members.
+
+YOU ARE SOLELY RESPONSIBLE FOR YOUR INTERACTIONS WITH OTHER MEMBERS. YOU UNDERSTAND THAT GOINGTO DOES NOT CONDUCT CRIMINAL BACKGROUND CHECKS ON ITS MEMBERS OR OTHERWISE INQUIRE INTO THE BACKGROUND OF ITS MEMBERS. GOINGTO MAKES NO REPRESENTATIONS OR WARRANTIES AS TO THE CONDUCT OF MEMBERS.</h4>
+                        
+                        <h2 style="margin-top: 20px; margin-bottom: 5px">6. Rights that GoingTo grants
+                                    you.</h2>
+                        <h4 style="font-weight: normal">GoingTo grants you a personal, worldwide, royalty-free, non-assignable, nonexclusive, revocable, and non-sublicensable license to access and use the Service. This license is for the sole purpose of letting you use and enjoy the Service’s benefits as intended by GoingTo and permitted by this Agreement. Therefore, you agree not to:
+
+    <li>use the Service or any content contained in the Service for any commercial purposes without our written consent.</li>
+    <li>copy, modify, transmit, create any derivative works from, make use of, or reproduce in any way any copyrighted material, images, trademarks, trade names, service marks, or other intellectual property, content or proprietary information accessible through the Service without GoingTo’s prior written consent.</li>
+    <li>express or imply that any statements you make are endorsed by GoingTo.</li>
+    <li>use any robot, bot, spider, crawler, scraper, site search/retrieval application, proxy or other manual or automatic device, method or process to access, retrieve, index, "data mine", or in any way reproduce or circumvent the navigational structure or presentation of the Service or its contents.</li>
+    <li>use the Service in any way that could interfere with, disrupt or negatively affect the Service or the servers or networks connected to the Service.</li>
+    <li>upload viruses or other malicious code or otherwise compromise the security of the Service.</li>
+    <li>forge headers or otherwise manipulate identifiers in order to disguise the origin of any information transmitted to or through the Service.</li>
+    <li>"frame" or "mirror" any part of the Service without GoingTo’s prior written authorization.</li>
+    <li>use meta tags or code or other devices containing any reference to GoingTo or the Service (or any trademark, trade name, service mark, logo or slogan of GoingTo) to direct any person to any other website for any purpose.</li>
+    <li>modify, adapt, sublicense, translate, sell, reverse engineer, decipher, decompile or otherwise disassemble any portion of the Service, or cause others to do so.</li>
+    <li>use or develop any third-party applications that interact with the Service or other members' Content or information without our written consent.</li>
+    <li>use, access, or publish the GoingTo application programming interface without our written consent.</li>
+    <li>probe, scan or test the vulnerability of our Service or any system or network.</li>
+    <li>encourage or promote any activity that violates this Agreement.</li>
+
+The Company may investigate and take any available legal action in response to illegal and/ or unauthorized uses of the Service, including termination of your account.
+
+Any software that we provide you may automatically download and install upgrades, updates, or other new features. You may be able to adjust these automatic downloads through your device's settings.</h4>
+                        
+                        <h2 style="margin-top: 20px; margin-bottom: 5px">7. Rights that you grant to
+                                    GoingTo.</h2>
+                        <h4 style="font-weight: normal">By creating an account, you grant to GoingTo a worldwide, transferable, sub-licensable, royalty-free, right and license to host, store, use, copy, display, reproduce, adapt, edit, publish, modify and distribute information you authorize us to access from Facebook, as well as any information you post, upload, display or otherwise make available (collectively, "post") on the Service or transmit to other members (collectively, "Content"). GoingTo's license to your Content shall be non-exclusive, except that GoingTo's license shall be exclusive with respect to derivative works created through use of the Service. For example, GoingTo would have an exclusive license to screenshots of the Service that include your Content. In addition, so that GoingTo can prevent the use of your Content outside of the Service, you authorize GoingTo to act on your behalf with respect to infringing uses of your Content taken from the Service by other members or third parties. This expressly includes the authority, but not the obligation, to send notices pursuant to 17 U.S.C. § 512(c)(3) (i.e., DMCA Takedown Notices) on your behalf if your Content is taken and used by third parties outside of the Service. Our license to your Content is subject to your rights under applicable law (for example laws regarding personal data protection to the extent any Content contains personal information as defined by those laws) and is for the limited purpose of operating, developing, providing, and improving the Service and researching and developing new ones. You agree that any Content you place or that you authorize us to place on the Service may be viewed by other members and may be viewed by any person visiting or participating in the Service (such as individuals who may receive shared Content from other GoingTo members).
+
+You agree that all information that you submit upon creation of your account, including information submitted from your Facebook account, is accurate and truthful and you have the right to post the Content on the Service and grant the license to GoingTo above.
+
+You understand and agree that we may monitor or review any Content you post as part of a Service. We may delete any Content, in whole or in part, that in our sole judgment violates this Agreement or may harm the reputation of the Service.
+
+When communicating with our customer care representatives, you agree to be respectful and kind. If we feel that your behavior towards any of our customer care representatives or other employees is at any time threatening or offensive, we reserve the right to immediately terminate your account.
+
+In consideration for GoingTo allowing you to use the Service, you agree that we, our affiliates, and our third-party partners may place advertising on the Service. By submitting suggestions or feedback to GoingTo regarding our Service, you agree that GoingTo may use and share such feedback for any purpose without compensating you.
+
+Please be informed that GoingTo may access, store and disclose your account information and Content if required to do so by law, by performing its agreement with you, or in a good faith belief that such access, storage or disclosure satisfies a legitimate interest, including to: (i) comply with legal process; (ii) enforce the Agreement; (iii) respond to claims that any Content violates the rights of third parties; (iv) respond to your requests for customer service; or (v) protect the rights, property or personal safety of the Company or any other person.</h4>
+                        
+                        <h2 style="margin-top: 20px; margin-bottom: 5px">8. 8. Community rules.</h2>
+                        <h4 style="font-weight: normal">By using the Service, you agree that you will not:
+
+    <li>use the Service for any purpose that is illegal or prohibited by this Agreement.</li>
+    <li>use the Service for any harmful or nefarious purpose</li>
+    <li>use the Service in order to damage GoingTo</li>
+    <li>violate our Community Guidelines, as updated from time to time.</li>
+    <li>spam, solicit money from or defraud any members.</li>
+    <li>impersonate any person or entity or post any images of another person without his or her permission.</li>
+    <li>bully, "stalk", intimidate, assault, harass, mistreat or defame any person.</li>
+    <li>post any Content that violates or infringes anyone's rights, including rights of publicity, privacy, copyright, trademark or other intellectual property or contract right.</li>
+    <li>post any Content that is hate speech, threatening, sexually explicit or pornographic.</li>
+    <li>post any Content that incites violence; or contains nudity or graphic or gratuitous violence.</li>
+    <li>post any Content that promotes racism, bigotry, hatred or physical harm of any kind against any group or individual.</li>
+    <li>solicit passwords for any purpose, or personal identifying information for commercial or unlawful purposes from other members or disseminate another person's personal information without his or her permission.</li>
+    <li>use another member's account, share an account with another member, or maintain more than one account.</li>
+    <li>create another account if we have already terminated your account, unless you have our permission.</li>
+
+GoingTo reserves the right to investigate and/ or terminate your account without a refund of any purchases if you have violated this Agreement, misused the Service or behaved in a way that GoingTo regards as inappropriate or unlawful, including actions or communications that occur on or off the Service.</h4>
                     </v-col>
                 </v-row>
             </div>
